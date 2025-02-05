cd0e7a8119e07b6df9eae9ff22c02ff21fae2db1
stephenraj22 <xerananymous33@google.com>
2025-02-05 22:18:20 +0530

    create url api fix2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace with your React app's URL
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your React app's URL
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
