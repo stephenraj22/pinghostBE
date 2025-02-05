e3d5773c7d0ec58349ecbbc368a6d36e201ca114
stephenraj22 <xerananymous33@google.com>
2025-02-05 22:05:43 +0530

    create url api

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const thread = require("./routes/thread/Thread");
 const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://site.com'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  })
+);
 app.use(express.json());
 app.use("/api/topic", topic);
 app.use("/api/thread", thread);
